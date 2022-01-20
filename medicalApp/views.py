0523776ceb124fcdc9b0481c18b0d4bb586e85f9
ismael jonatas <jhons1808@outlook.com>
2022-01-20 18:55:39 -0300

    Tela index feita. iniciando tela de cadastro usuario

--- medicalApp/views.py
+++ medicalApp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def cadastro_usuario(request):
+    return render(request, 'cadastro-usuario.html')
