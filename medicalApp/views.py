18bc06c72edaf06e160478798038a6f3b6b2f962
ismael jonatas <jhons1808@outlook.com>
2022-01-20 23:42:12 -0300

    Login é cadastro de user feitos, config login request.sessions

--- medicalApp/views.py
+++ medicalApp/views.py
@@ -1,11 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import auth
+
+from .models import Usuario
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def cadastro_usuario(request):
-    return render(request, 'cadastro-usuario.html')
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        senha2 = request.POST['senha2']
+        data = request.POST['data']
+
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('cadastro_usuario')
+        if senha != senha2:
+            print('As senhas não são iguais')
+            return redirect('cadastro_usuario')
+        if Usuario.objects.filter(email=email).exists():
+            print('Usuário já cadastrado')
+            return redirect('cadastro_usuario')
+
+        user = Usuario.objects.create(nome=nome, email=email, senha=senha, data_criacao = data)
+        user.save()
+        print('cadastrado')
+        return redirect('login')
+    else:
+         return render(request, 'cadastro-usuario.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        m = Usuario.objects.get(email=request.POST['email'])
+        if m.senha == request.POST['senha']:
+            request.session['member_id'] = m.id
+            return redirect('index')
+        else:
+            print('Usuario inexistente')
+            return redirect('cadastro_usuario')   
+    else:
+        return render(request, 'login.html')
+
+def logout (request):
+    try:
+        del request.session['member_id']
+    except KeyError:
+        pass
+    return redirect('index')
